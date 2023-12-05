f37091225cb76c3b52058d741149c0cf3fcebefb
Srdan Prodanovic <srprodan@microsoft.com>
2023-12-05 11:06:02 +0100

    day 2 cmplet

--- src/aoc/1202/cube.py
+++ src/aoc/1202/cube.py
@@ -6,9 +6,22 @@ constraints = {"red":12,  "green":13,  "blue":14}
 with open(os.path.join(os.getcwd(), 'src\\aoc\\1202\\input.txt'), 'r') as input:
         lines = input.readlines()
 
+sum = 0
 for line in lines:
         i = int(line[4:line.find(":")])
         line = line.split(":")[1]
-        for group in line.split(";"):
-                for cells in group.split(","):
-                        print(cells)
+        possible = True
+        for gameset in line.split(";"):                
+                for cell in gameset.split(","):
+                        cell = cell.strip()
+                        val = int(cell.split(" ")[0].strip())
+                        color = cell.split(" ")[1].strip()
+                        # print(f"{color}:{val}")
+                        if color in constraints.keys():
+                                if constraints[color] < val: possible = False
+                        else: possible = False
+        if possible: sum += i
+
+print (sum)
+
+
